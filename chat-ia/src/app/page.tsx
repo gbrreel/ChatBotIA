ddccd7587b252f5e3f9dc8b406b690b156a52aab
gbrreel <gabrielggalloo@gmail.com>
2025-01-27 22:44:13 -0300

    Inicio desenvolvimento tela

--- chat-ia/src/app/page.tsx
+++ chat-ia/src/app/page.tsx
@@ -1,5 +1,22 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../../src/components/ui/card';
+
 export default function Home() {
   return (
-    <h1>hello </h1>
+
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className='w-[440px] h-[700px]'>
+        <CardHeader>
+          <CardTitle>Chat IA</CardTitle>
+          <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mensagens</p>
+        </CardContent>
+        <CardFooter>
+          Form  
+        </CardFooter>
+      </Card>
+    </div>
+
   );
 }
